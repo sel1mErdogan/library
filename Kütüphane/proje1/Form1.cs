774f0ad5cb9621460b692cdbf8fd759064f6b860
selim <121359078+sel1mErdogan@users.noreply.github.com>
2023-06-09 00:25:35 +0300

    Add files via upload

---
+++ Kütüphane/proje1/Form1.cs
@@ -0,0 +1,308 @@
+namespace proje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+        Random random=new Random();
+
+        int sayac = 0;
+        int oran = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            oran = 0;
+            sayac = 0;
+            int rast= random.Next(0,2);
+            int rast2 = random.Next(0, 2);
+            int rast3 = random.Next(0, 2);
+            int rast4 = random.Next(0, 2);
+            int rast5 = random.Next(0, 2);
+            int rast6 = random.Next(0, 2);
+            int rast7 = random.Next(0, 2);
+            int rast8 = random.Next(0, 2);
+            int rast9 = random.Next(0, 2);
+            int rast10 = random.Next(0, 2);
+            int rast11 = random.Next(0, 2);
+            int rast12 = random.Next(0, 2);
+            int rast13 = random.Next(0, 2);
+            int rast14 = random.Next(0, 2);
+            int rast15 = random.Next(0, 2);
+            int rast16 = random.Next(0, 2);
+            int rast17 = random.Next(0, 2);
+            int rast18 = random.Next(0, 2);
+            int rast19 = random.Next(0, 2);
+            int rast20 = random.Next(0, 2);
+            int rast21 = random.Next(0, 2);
+            int rast22 = random.Next(0, 2);
+            int rast23 = random.Next(0, 2);
+            int rast24 = random.Next(0, 2);
+
+            if (rast == 1)
+                pictureBox1.BackColor = Color.Red;
+            else { 
+                pictureBox1.BackColor = Color.Lime;
+                sayac++;
+            }
+            if (rast2 == 1)          
+                pictureBox2.BackColor = Color.Red; 
+            else{
+                pictureBox2.BackColor = Color.Lime;sayac++;
+            }
+            if (rast3 == 1)            
+                pictureBox5.BackColor = Color.Red;           
+            else{          
+                pictureBox5.BackColor = Color.Lime;sayac++;
+            }
+            if (rast4 == 1)           
+                pictureBox4.BackColor = Color.Red;           
+            else{           
+                pictureBox4.BackColor = Color.Lime; sayac++;
+            }
+            if (rast5 == 1)           
+                pictureBox10.BackColor = Color.Red;           
+            else{           
+                pictureBox10.BackColor = Color.Lime; sayac++;
+            }
+            if (rast6 == 1)          
+                pictureBox8.BackColor = Color.Red;           
+            else{          
+                pictureBox8.BackColor = Color.Lime; sayac++;
+                }
+            if (rast7 == 1)           
+                pictureBox7.BackColor = Color.Red;           
+            else{            
+                pictureBox7.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast8 == 1)          
+                pictureBox6.BackColor = Color.Red;           
+            else{           
+                pictureBox6.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast9 == 1)
+                pictureBox40.BackColor = Color.Red;
+            else{
+                pictureBox40.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast10 == 1)
+                pictureBox38.BackColor = Color.Red;
+            else{
+                pictureBox38.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast11 == 1)
+                pictureBox37.BackColor = Color.Red;
+            else{
+                pictureBox37.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast12 == 1)
+                pictureBox36.BackColor = Color.Red;
+            else{
+                pictureBox36.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast13 == 1)
+                pictureBox35.BackColor = Color.Red;
+            else{
+                pictureBox35.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast14 == 1)
+                pictureBox35.BackColor = Color.Red;
+            else{
+                pictureBox33.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast15 == 1)
+                pictureBox32.BackColor = Color.Red;
+            else{
+                pictureBox32.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast16 == 1)
+                pictureBox31.BackColor = Color.Red;
+            else{
+                pictureBox31.BackColor = Color.Lime; sayac++;
+                    }
+            if (rast2 == 1)
+                pictureBox56.BackColor = Color.Red;
+            else{
+                pictureBox56.BackColor = Color.Lime; sayac++;
+                }
+            if (rast3 == 1)
+                pictureBox58.BackColor = Color.Red;
+            else{
+                pictureBox58.BackColor = Color.Lime; sayac++;
+                }
+            if (rast4 == 1)
+                pictureBox60.BackColor = Color.Red;
+            else{
+                pictureBox60.BackColor = Color.Lime; sayac++;
+                }
+            if (rast5 == 1)
+                pictureBox51.BackColor = Color.Red;
+            else{
+                pictureBox51.BackColor = Color.Lime; sayac++;
+                }
+            if (rast6 == 1)
+                pictureBox52.BackColor = Color.Red;
+            else{
+                pictureBox52.BackColor = Color.Lime; sayac++;
+                }
+            if (rast7 == 1)
+                pictureBox53.BackColor = Color.Red;
+            else{
+                pictureBox53.BackColor = Color.Lime; sayac++;
+                }
+            if (rast8 == 1)
+                pictureBox55.BackColor = Color.Red;
+            else{
+                pictureBox55.BackColor = Color.Lime; sayac++;
+                }
+            if (rast9 == 1)
+                pictureBox16.BackColor = Color.Red;
+            else{
+                pictureBox16.BackColor = Color.Lime; sayac++;
+                }
+            if (rast10 == 1)
+                pictureBox17.BackColor = Color.Red;
+            else{
+                pictureBox17.BackColor = Color.Lime; sayac++;
+                }
+            if (rast11 == 1)
+                pictureBox18.BackColor = Color.Red;
+            else{
+                pictureBox18.BackColor = Color.Lime; sayac++;
+                }
+            if (rast12 == 1)
+                pictureBox20.BackColor = Color.Red;
+            else{
+                pictureBox20.BackColor = Color.Lime; sayac++;
+                }
+            if (rast13 == 1)
+                pictureBox12.BackColor = Color.Red;
+            else{
+                pictureBox12.BackColor = Color.Lime; sayac++;
+                }
+            if (rast14 == 1)
+                pictureBox13.BackColor = Color.Red;
+            else{
+                pictureBox13.BackColor = Color.Lime; sayac++;
+                }
+            if (rast15 == 1)
+                pictureBox15.BackColor = Color.Red;
+            else{
+                pictureBox15.BackColor = Color.Lime; sayac++;
+                }
+            if (rast16 == 1)
+                pictureBox11.BackColor = Color.Red;
+            else{
+                pictureBox11.BackColor = Color.Lime; sayac++;
+                }
+            if (rast13 == 1)
+                pictureBox26.BackColor = Color.Red;
+            else{
+                pictureBox26.BackColor = Color.Lime; sayac++;
+                }
+            if (rast14 == 1)
+                pictureBox27.BackColor = Color.Red;
+            else{
+                pictureBox27.BackColor = Color.Lime; sayac++;
+                }
+            if (rast15 == 1)
+                pictureBox28.BackColor = Color.Red;
+            else{
+                pictureBox28.BackColor = Color.Lime; sayac++;
+                }
+            if (rast16 == 1)
+                pictureBox21.BackColor = Color.Red;
+            else{
+                pictureBox21.BackColor = Color.Lime; sayac++;
+                }
+            if (rast13 == 1)
+                pictureBox30.BackColor = Color.Red;
+            else{
+                pictureBox30.BackColor = Color.Lime; sayac++;
+                }
+            if (rast14 == 1)
+                pictureBox22.BackColor = Color.Red;
+            else{
+                pictureBox22.BackColor = Color.Lime; sayac++;
+                }
+            if (rast15 == 1)
+                pictureBox23.BackColor = Color.Red;
+            else{
+                pictureBox23.BackColor = Color.Lime; sayac++;
+                }
+            if (rast16 == 1)
+                pictureBox25.BackColor = Color.Red;
+            else{
+                pictureBox25.BackColor = Color.Lime; sayac++;
+                }
+            if (rast17 == 1)
+                pictureBox46.BackColor = Color.Red;
+            else{
+                pictureBox46.BackColor = Color.Lime; sayac++;
+                }
+            if (rast18 == 1)
+                pictureBox47.BackColor = Color.Red;
+            else{
+                pictureBox47.BackColor = Color.Lime; sayac++;
+                }
+            if (rast19 == 1)
+                pictureBox48.BackColor = Color.Red;
+            else{
+                pictureBox48.BackColor = Color.Lime; sayac++;
+                }
+            if (rast20 == 1)
+                pictureBox50.BackColor = Color.Red;
+            else{
+                pictureBox50.BackColor = Color.Lime; sayac++;
+                }
+            if (rast21 == 1)
+                pictureBox41.BackColor = Color.Red;
+            else{
+                pictureBox41.BackColor = Color.Lime; sayac++;
+                }
+            if (rast22 == 1)
+                pictureBox42.BackColor = Color.Red;
+            else{
+                pictureBox42.BackColor = Color.Lime; sayac++;
+                }
+            if (rast23 == 1)
+                pictureBox43.BackColor = Color.Red;
+            else{
+                pictureBox43.BackColor = Color.Lime; sayac++;
+                }
+            if (rast24 == 1)
+                pictureBox45.BackColor = Color.Red;
+            else{
+                pictureBox45.BackColor = Color.Lime; sayac++;
+               } 
+                
+            oran = (100 * sayac) / -48 +100 ;
+            label13.Text =  "%"+oran+" Dolu" ;
+            label15.Text = "salondaki kisi sayisi= " + (-sayac+48);
+            progressBar1.Value = 48-sayac;
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
